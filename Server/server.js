5cb299ad9ce70314e4a63cf6825c05c60c94d022
Ansari Irfan <115148633+Ansari-Irfan-360@users.noreply.github.com>
2024-06-05 22:20:00 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const userRoute = require('./Routes/userRoute.js')
 const cors = require('cors')
 const dotenv = require('dotenv')
-dotenv.config({ path: "./Config/.env" })
-const connectDB = require('./Config/database.js');
+dotenv.config({ path: "./.env" })
+const connectDB = require('./database.js');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
